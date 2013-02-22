d63e229e7d24ee4148a0987fa6e3ad484dfbc286
local <local@localhost>
2013-02-22 15:41:16 +0900

    update

--- scala1/src/jdbc/JdbcTest.scala
+++ scala1/src/jdbc/JdbcTest.scala
@@ -9,13 +9,25 @@ object JdbcTest {
   def main(args: Array[String]) {
     val url = "jdbc:derby:memory:test;create=true"
 
-    using(DriverManager.getConnection(url)) { conn =>
-      using(conn.createStatement()) { stmt =>
-        stmt.execute("CREATE TABLE t_test (s VARCHAR(8),n INTEGER)")
-        stmt.executeUpdate("INSERT INTO t_test VALUES('John',1)")
-        stmt.executeUpdate("INSERT INTO t_test VALUES('Allen',2)")
-        stmt.executeUpdate("INSERT INTO t_test VALUES('Marcel',3)")
-      }
+    using(DriverManager.getConnection(url)) {
+      conn =>
+        using(conn.createStatement()) {
+          stmt =>
+            stmt.execute("CREATE TABLE t_test (s VARCHAR(8),n INTEGER)")
+            stmt.executeUpdate("INSERT INTO t_test VALUES('John',1)")
+            stmt.executeUpdate("INSERT INTO t_test VALUES('Allen',2)")
+            stmt.executeUpdate("INSERT INTO t_test VALUES('Marcel',3)")
+        }
+        using(conn.prepareStatement("SELECT * FROM t_test WHERE n=?")) {
+          stmt => 
+            stmt.setInt(1, 3)
+            using(stmt.executeQuery()) {
+              rs => 
+                while (rs.next)
+                  println(rs.getString("s"))
+            }
+
+        }
     }
   }
 }
