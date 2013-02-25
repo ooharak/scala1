1f110b73019c3a98037f4efd68766f68a0b3d3a8
local <local@localhost>
2013-02-25 16:54:06 +0900

    init

---
+++ scala1/src/breakable/Breakability.scala
@@ -0,0 +1,116 @@
+package breakable
+
+import scala.util.control.Breaks.{ break, breakable }
+
+/**
+ * もともとScalaのbreakableというやつを使ってみる例だったけれど、
+ * whileを再帰で置き換えてみるときに無名関数の再帰をしたいがために
+ * Yコンビネータなるものを試してみた記録。
+ *
+ */
+object Breakability {
+
+  /**
+   * breakable というやつを使ってみる例
+   */
+  def useBreak() {
+    var n = 0
+    breakable {
+      while (true) {
+        println(n)
+        n += 1
+        if (n >= 5) break
+      }
+    }
+  }
+
+  /**
+   * Y combinator (http://yuroyoro.hatenablog.com/entry/20100204/1265285318)
+   *
+   * 実装動機は次の通り：
+   *
+   * 「再帰を含む無名関数を定義して使いたい（ただし定義の中では一時的に名前が要る）」
+   * 2013-02-25: 再帰の最適化がどうなるのかよくわかってない。
+   *
+   *
+   * 関数渡しは関数が実際に適用されるまで評価されないのは当たり前なんだけど混乱するので注意。
+   * 色々と頭が固いので、まずはScalaの定義をHaskell風に換えておいて考えてみたい。
+   *
+   * y::((a->b)->a->b)->a->b
+   * y f x = f (\z -> (y f z)) x
+   *
+   * 之に対して例えば以下を適用するわけだ。
+   *
+   * result = y inner (0,"z")
+   *          where inner _ (5,s) = s
+   *                inner g (n,s) = g (n+1, (show n)++s)
+   * {-
+   *   result
+   *   → y inner (0,"z")
+   *   → inner (\z->(y inner z)) (0,"z")
+   *   → (\z->(y inner z)) $ (0+1, (show 0)++"z")
+   *   → y inner (1, "0z")
+   *   → ...
+   *   → y inner (5, "43210z")
+   *   → inner (\z->(y inner z)) (5, "43210z")
+   *   → "43210z"
+   * -}
+   *
+   * yに渡すfは「計算をするし、関数gを貰ってそいつにこの例なら次のタプルを渡して再帰を下らせる」。
+   * yはfと初期値xを貰ってfにxと自作のgを渡す。このgは次のタプルを貰ってy自身を再帰呼び出す。
+   *
+   * 再帰できるのはわかったけど、ラムダ計算との同値性がわからない。
+   * yの定義にyが登場しているのがなんとなく反則っぽいのかな。というのも
+   * Wikipediaに載ってるハスケル・カリーの定義
+   * Y = λf . (λx . f (x x)) (λx . f (x x))
+   *
+   * ではイコールの右辺にYが出てこない。ラムダ計算では再帰的定義しちゃいけないらしいので。
+   *
+   * あとこの、f (x x)がよくわからない。
+   * ラムダ計算でx xというのは関数xの引数にxを与えるという意味らしいんだけど
+   * 何の為にこんなことしてんの
+   *
+   *
+   * @param f
+   * @param x
+   * @return
+   */
+  def y[A, B](f: ((A => B), A) => B, x: A): B =
+    f((z: A) => y(f, z), x)
+
+  def dontuseBreak() {
+    y((g: Int => Int, n: Int) =>
+      n match {
+        case 5 => n
+        case _ => println(n); g(n + 1)
+      }, 0)
+  }
+
+  /**
+   * よくわからないので分解
+   */
+  def dontuseBreakExplained() {
+    def inner(g: Int => Int, n: Int) = {
+      n match {
+        case 5 =>
+          println("end"); n
+        case _ => println(n); g(n + 1)
+      }
+    }
+    y(inner, 0)
+    // -->  [1] inner((z:A)=>y(inner,z), 0)
+    // -->  [2] g(0+1) //innerを展開
+    // -->  [3] println(1); y(inner, 1)
+    // -->  ...
+    // -->  [4] y(inner, 5)
+    // -->  [5] println("end"); 5
+  }
+
+  def main(args: Array[String]) {
+    useBreak()
+    println("---")
+    dontuseBreak()
+    println("---")
+    dontuseBreakExplained()
+  }
+}
